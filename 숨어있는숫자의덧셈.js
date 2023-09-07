1223a020892d634463292a55b26432c553a9199b
SeoJumee <official.jumee@gmail.com>
2023-09-07 23:14:45 +0900

    숨어있는 숫자의 덧셈(1)

---
+++ 숨어있는숫자의덧셈.js
@@ -0,0 +1,7 @@
+function solution(my_string) {
+  let answer = 0;
+  my_string.split('').map((e) => (!isNaN(e) ? (answer += Number(e)) : 0));
+  return answer;
+}
+
+// 문자열 my_string이 매개변수로 주어집니다. my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
