533c9cb7ed5352a50035c668bf4901825ae360ae
SeoJumee <official.jumee@gmail.com>
2024-01-30 18:36:00 +0900

    나누어 떨어지는 숫자 배열

---
+++ Lv.1/나누어떨어지는숫자배열.js
@@ -0,0 +1,10 @@
+function solution(arr, divisor) {
+  var answer = [];
+  arr.map((i) => {
+    i % divisor === 0 && answer.push(i);
+  });
+  return answer.length ? answer.sort((a, b) => a - b) : [-1];
+}
+
+// array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
+// divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
