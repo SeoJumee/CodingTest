1f9b6dd466bc97d6ee6d4004f701079d92d03800
SeoJumee <official.jumee@gmail.com>
2024-02-04 23:48:58 +0900

    조건에 맞게 수열 변환하기 1

---
+++ Lv.0/조건에맞게수열변환하기1.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+  return arr.map((a) => {
+    if (a >= 50 && a % 2 === 0) return Math.floor(a / 2);
+    if (a < 50 && a % 2 === 1) return a * 2;
+    return a;
+  });
+}
+
+// 정수 배열 arr가 주어집니다. arr의 각 원소에 대해 값이 50보다 크거나 같은 짝수라면 2로 나누고, 50보다 작은 홀수라면 2를 곱합니다. 그 결과인 정수 배열을 return 하는 solution 함수를 완성해 주세요.정수 배열 arr가 주어집니다. arr의 각 원소에 대해 값이 50보다 크거나 같은 짝수라면 2로 나누고, 50보다 작은 홀수라면 2를 곱합니다. 그 결과인 정수 배열을 return 하는 solution 함수를 완성해 주세요.
