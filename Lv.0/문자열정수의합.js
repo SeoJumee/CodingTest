be84476798d9cdbf7e302005a38235f2f6759087
SeoJumee <official.jumee@gmail.com>
2024-02-19 23:44:44 +0900

    문자열 정수의 합

---
+++ Lv.0/문자열정수의합.js
@@ -0,0 +1,5 @@
+function solution(num_str) {
+  return num_str.split('').reduce((acc, curr) => acc + Number(curr), 0);
+}
+
+// 한 자리 정수로 이루어진 문자열 num_str이 주어질 때, 각 자리수의 합을 return하도록 solution 함수를 완성해주세요.
