bd7ccead74843fb1996da8465068ab1f537a41ec
SeoJumee <official.jumee@gmail.com>
2024-04-07 23:05:01 +0900

    약수의 개수와 덧셈

---
+++ Lv.1/약수의개수와덧셈.js
@@ -0,0 +1,28 @@
+function solution(left, right) {
+  let arr = [];
+  let answer = 0;
+
+  for (let i = left; i <= right; i++) {
+    for (let j = 1; j <= i; j++)
+      if (i % j === 0) {
+        answer++;
+      }
+    arr.push(answer % 2 ? -i : i);
+    answer = 0;
+  }
+  return arr.reduce((acc, cur) => acc + cur, 0);
+}
+
+function solution(left, right) {
+  let answer = 0;
+  for (let i = left; i <= right; i++) {
+    if (Number.isInteger(Math.sqrt(i))) {
+      answer -= i;
+    } else {
+      answer += i;
+    }
+  }
+  return answer;
+}
+
+// 두 정수 left와 right가 매개변수로 주어집니다. left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
